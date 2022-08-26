18fec700ac651fe26e182c559fe33b9f76cd4d12
Shauna MacFarlane-Okongo <102887963+DrSLMac@users.noreply.github.com>
2022-08-25 23:15:33 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react"
+import Ideas from './Ideas'
+import './App.css'
+import Form from './Form'
+
+class App extends Component {
+  constructor() {
+    super() 
+    this.state = {
+      ideas: [
+        { id: 1, title: 'Prank Travis', description: 'Stick googly eyes on all his stuff' },
+        { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+        { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' },
+      ]
+    }
+
+    this.addIdea = this.addIdea.bind(this)
+  }
+
+  addIdea = (newIdea) => {
+    this.setState({ ideas: [...this.state.ideas, newIdea]})
+  }
+
+  deleteIdea = (id) => {
+    console.log(id);
+    const filteredIdeas = this.state.ideas.filter(idea => idea.id != id)
+
+    this.setState({ ideas: filteredIdeas})
+  }
+
+  render() {
+    return (
+      <main className='App'>
+        <h1>IdeaBox</h1>
+        <p>HOWDY!</p>
+        <Form addIdea={this.addIdea} />
+        <Ideas ideas={this.state.ideas} deleteIdea={this.deleteIdea}/>
+      </main>
+    )
+  }
+}
+
+export default App
